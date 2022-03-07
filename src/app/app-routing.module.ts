ebe801af91cc9eb761bfad3b70aec4afb37d5223
gorkem <gorkemyavas98@gmail.com>
2022-03-07 18:26:15 +0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomepageComponent} from "./views/homepage/homepage.component";
+import {PostsComponent} from "./views/posts/posts.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomepageComponent
+  },
+  {
+    path:'posts',
+    component:PostsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomepageComponent } from './views/homepage/homepage.component';
+import {HttpClientModule} from "@angular/common/http";
+import { PostsComponent } from './views/posts/posts.component';
+import {NgxPaginationModule} from "ngx-pagination";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HeaderComponent,
+    HomepageComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
